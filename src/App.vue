2a071ffe3490320396bed6cb09b966609abe4be6
the_gt99@mail.ru <Qweasdzxc1230!!>
2024-02-16 17:13:08 +0400

    first

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar/>
+</template>
+
+<script>
+import NavBar from "./components/NavBar";
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+</style>
